f80ab1f0c8dc0328ef4e09e63cd1ba30964698b8
dougtq <douglas.expalves@gmail.com>
2019-05-29 01:42:10 -0300

    chore: rmv arquivos de build cargo

--- hello_cargo/target/debug/deps/hello_cargo-bf4b80c50a057617.d
+++
@@ -1,5 +0,0 @@
-C:\Users\dougl\Desktop\Code\rust-lang\hello_cargo\target\debug\deps\hello_cargo-bf4b80c50a057617.exe: src\main.rs
-
-C:\Users\dougl\Desktop\Code\rust-lang\hello_cargo\target\debug\deps\hello_cargo-bf4b80c50a057617.d: src\main.rs
-
-src\main.rs:
