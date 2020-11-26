e26a4225a4bf477065c002ec9f4a0d3fa77f29d1
dougtq <douglas.expalves@gmail.com>
2020-11-26 10:51:19 -0300

    feat: test values and references

--- welcome_back/src/main.rs
+++ welcome_back/src/main.rs
@@ -7,4 +7,16 @@ fn main() {
     println!("Hello, world!");
     temporario();
     var();
+    var_test();
+}
+
+
+fn var_test() {
+    let mut a = 1;
+    let vetor = vec![1, 3, 4];
+
+    a += 10;
+
+    println!("{}", a);
+    println!("{}", vetor[1]);
 }
