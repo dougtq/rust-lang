2965caf5caec42da5c8b556a9e9ad0cb0b9ddcb0
Douglas Alves <douglas.alves@blu365.com.br>
2021-01-20 22:51:16 -0300

    using filter and fold/reduce

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -5,14 +5,22 @@ fn main() {
 
     println!("Enum {:?}", enum_it.next());
 
-    let num_it = (1..10).into_iter();
+    let map_it = (1..10).into_iter();
+    let filter_it = (1..10).into_iter();
     let for_it = (1..10).into_iter();
+    let fold_it = (1..10).into_iter();
 
-    let other : Vec<i32> = num_it.map(|x| {
+    let other : Vec<i32> = map_it.map(|x| {
         x as i32 + 3
     }).collect();
 
     println!("Map {:?}", other);
+
+    let other_filtered : Vec<_> = filter_it.filter(|x| { x % 2 == 0 }).collect();
+
+    println!("Filter {:?}", other_filtered);
+
+    println!("Fold {:?}", fold_it.fold(0, |sum, item| { sum + item }));
 
     let mut other_items : Vec<i32> = Vec::new();
 
