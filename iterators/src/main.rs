ab9a097933c55f7079ce85de4ecbb8b5cbf0c91f
Douglas Alves <douglas.alves@blu365.com.br>
2021-01-20 22:07:29 -0300

    using for..in

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,16 +1,24 @@
 fn main() {
-    let mut it = vec!["A", "F", "D"].into_iter();
+    let it = vec!["A", "F", "D"].into_iter();
 
     let mut enum_it = it.enumerate();
 
     println!("Enum {:?}", enum_it.next());
 
-
     let num_it = (1..10).into_iter();
+    let for_it = (1..10).into_iter();
 
     let other : Vec<i32> = num_it.map(|x| {
         x as i32 + 3
     }).collect();
 
     println!("Map {:?}", other);
+
+    let mut other_items : Vec<i32> = Vec::new();
+
+    for item in for_it {
+        other_items.push(item + 3)
+    }
+    println!("for..in {:?}", other_items);
+
 }
