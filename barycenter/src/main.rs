94d80c4be31482a26b185524a2f6fa3bfaf9d510
Douglas Alves <douglas.alves@blu365.com.br>
2021-01-20 23:53:42 -0300

    creating barycenter example

---
+++ barycenter/src/main.rs
@@ -0,0 +1,53 @@
+extern crate rayon;
+extern crate itertools;
+
+use rayon::prelude::*;
+use itertools::Itertools;
+
+mod bodies;
+//use bodies
+
+#[derive(Debug, Clone, Copy)]
+pub struct Body {
+    x: f64,
+    y: f64,
+    z: f64,
+    mass: f64
+}
+
+fn average (a: f64, b: f64) -> f64 {
+    (a + b) / 2.0
+}
+
+fn average_with_mass (a: f64, b: f64, a_mass: f64, b_mass: f64) -> f64 {
+    average(a * a_mass, b * b_mass) / (a_mass + b_mass)
+}
+
+fn merge_bodies (a: Body, b: Body) -> Body {
+    Body {
+        x: average_with_mass(a.x, b.x, a.mass, b.mass),
+        y: average_with_mass(a.y, b.y, a.mass, b.mass),
+        z: average_with_mass(a.z, b.z, a.mass, b.mass),
+        mass: a.mass + b.mass
+    }
+}
+
+fn merge_all_bodies_iter (bodies: &[Body]) -> Body {
+    let barycenter : Body = bodies[0];
+
+    bodies.iter().skip(1).fold(barycenter, |barycenter, body|{
+        merge_bodies(barycenter, *body)
+    })
+
+
+}
+
+// fn merge_all_bodies_recursive () -> Body {}
+
+fn main() {
+    let bodies = bodies::get_values();
+
+    let barycenter = merge_all_bodies_iter(&bodies);
+
+    println!("Barycenter is {:?}", barycenter);
+}
